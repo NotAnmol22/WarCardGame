276d3e341f2b3b1c395ab5e62e61b2836ad9b0e7
NotAnmol22 <verma87@sheridancollege.ca>
2024-12-06 21:50:50 +0530

    Update War.java

--- War.java
+++ War.java
@@ -1,13 +1,20 @@
 package ca.sheridancollege.project;
 
+/**
+ * Represents the War card game.
+ * Implements the Strategy Pattern for scoring logic.
+ */
 public class War extends Game {
     private GroupOfCards deck;
-    private int player1Score;
-    private int player2Score;
+    private int player1Score = 0;
+    private int player2Score = 0;
+    private final int maxRounds = 4;
+    private ScoringStrategy scoringStrategy;
 
-    public War() {
+    public War(ScoringStrategy scoringStrategy) {
         super("War");
-        deck = new GroupOfCards();
+        this.scoringStrategy = scoringStrategy;
+        this.deck = new GroupOfCards();
         initializeDeck();
         deck.shuffle();
     }
@@ -16,46 +23,59 @@ public class War extends Game {
         String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
         for (String suit : suits) {
             for (int value = 1; value <= 13; value++) {
-                deck.addCard(new PlayingCard(suit, value));
+                deck.addCard(CardFactory.createCard(suit, value));
             }
         }
     }
 
     @Override
     public void play() {
-        System.out.println("Starting the War game!");
+        System.out.println("Starting the War game with " + maxRounds + " rounds!");
 
-        while (deck.size() >= 2) {  
+        for (int round = 1; round <= maxRounds; round++) {
+            System.out.println("\nRound " + round);
+
+            if (deck.size() < 2) {
+                System.out.println("Not enough cards to continue.");
+                break;
+            }
+
             PlayingCard player1Card = (PlayingCard) deck.drawCard();
             PlayingCard player2Card = (PlayingCard) deck.drawCard();
 
             System.out.println(getPlayers().get(0).getName() + " drew: " + player1Card);
             System.out.println(getPlayers().get(1).getName() + " drew: " + player2Card);
 
-            if (player1Card.getValue() > player2Card.getValue()) {
-                System.out.println(getPlayers().get(0).getName() + " wins this round!");
-                player1Score++;
-            } else if (player1Card.getValue() < player2Card.getValue()) {
-                System.out.println(getPlayers().get(1).getName() + " wins this round!");
-                player2Score++;
-            } else {
-                System.out.println("It's a tie!");
-            }
+            compareCards(player1Card, player2Card);
         }
+
         declareWinner();
+    }
+
+    private void compareCards(PlayingCard player1Card, PlayingCard player2Card) {
+        int scoreChange = scoringStrategy.calculateScore(player1Card.getValue(), player2Card.getValue());
+        if (scoreChange > 0) {
+            player1Score++;
+            System.out.println(getPlayers().get(0).getName() + " wins this round!");
+        } else if (scoreChange < 0) {
+            player2Score++;
+            System.out.println(getPlayers().get(1).getName() + " wins this round!");
+        } else {
+            System.out.println("It's a tie!");
+        }
     }
 
     @Override
     public void declareWinner() {
-        System.out.println("Game Over!");
+        System.out.println("\nGame Over!");
         System.out.println("Final Score:");
         System.out.println(getPlayers().get(0).getName() + ": " + player1Score);
         System.out.println(getPlayers().get(1).getName() + ": " + player2Score);
 
         if (player1Score > player2Score) {
-            System.out.println(getPlayers().get(0).getName() + " is the overall winner!");
+            System.out.println(getPlayers().get(0).getName() + " wins the game!");
         } else if (player1Score < player2Score) {
-            System.out.println(getPlayers().get(1).getName() + " is the overall winner!");
+            System.out.println(getPlayers().get(1).getName() + " wins the game!");
         } else {
             System.out.println("The game ends in a tie!");
         }
